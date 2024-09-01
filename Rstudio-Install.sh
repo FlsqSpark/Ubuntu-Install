8fffd3b9e9342da1257f3f5f0ea5c924651eac41
FlsqSpark <76749515+FlsqSpark@users.noreply.github.com>
2024-09-01 14:59:08 -0400

    Update Rstudio-Install.sh

--- Rstudio-Install.sh
+++ Rstudio-Install.sh
@@ -4,11 +4,13 @@ sudo apt update
 
 sudo apt install r-base-core -y
 
-sudo apt install libcurl4-openssl-dev
+sudo apt install libcurl4-openssl-dev -y
+
+sudo apt install libxml2-dev -y
 
 wget https://download1.rstudio.org/electron/jammy/amd64/rstudio-2024.04.2-764-amd64.deb
 
-sudo apt install ./rstudio-2024.04.2-764-amd64.deb
+sudo apt install ./rstudio-2024.04.2-764-amd64.deb -y
 
 
 
