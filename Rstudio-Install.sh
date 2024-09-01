ce8373ea678cd088502e0e29557e6a3ab8e39779
FlsqSpark <76749515+FlsqSpark@users.noreply.github.com>
2024-09-01 16:12:25 -0400

    Update Rstudio-Install.sh

--- Rstudio-Install.sh
+++ Rstudio-Install.sh
@@ -8,6 +8,12 @@ sudo apt install libcurl4-openssl-dev -y
 
 sudo apt install libxml2-dev -y
 
+sudo apt install libharfbuzz-dev libfribidi-dev -y
+
+sudo apt install libfreetype6-dev libpng-dev libtiff5-dev libjpeg-dev -y
+
+sudo apt install libfontconfig1-dev
+
 wget https://download1.rstudio.org/electron/jammy/amd64/rstudio-2024.04.2-764-amd64.deb
 
 sudo apt install ./rstudio-2024.04.2-764-amd64.deb -y
